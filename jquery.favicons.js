5605e80c7ab694b8f3b50a97770d7542a17c956b
Christian Vuerings <vueringschristian@gmail.com>
2011-05-06 13:05:38 +0200

    Make the default JS file more readable by adding linebreaks. Also add some more information to the file (e.g. version number)

--- jquery.favicons.js
+++ jquery.favicons.js
@@ -1,17 +1,24 @@
 /**
- * Add favicon images to links on the page
- * @param {Object} config Configuration options
+ * jQuery Favicons Plug-in v0.1
+ * Add favicon images to links on a page
+ * 
+ * Copyright 2011, Christian Vuerings - http://denbuzze.com
+ * License: https://github.com/christianv/jquery-favicons/raw/master/LICENSE
  */
 jQuery.fn.favicons = function(config){
   var settings = jQuery.extend({
-    'classname': 'favicon',
-    'service': 'http://g.etfv.co/__URL__'
+    "classname": "favicon",
+    "service": "http://g.etfv.co/__URL__"
   }, config);
 
   // Return this object to maintain chainability
   return this.each(function(){
     var link = $(this);
-    var url = settings.service.replace('__URL__' , link.attr('href')).replace('__DOMAIN__', link.attr('href').replace('http://','').replace('https://', ''));
-    link.css('background-image', 'url(' + url + ')').addClass(settings.classname);
+    var url = settings.service
+                .replace("__URL__" , link.attr("href"))
+                .replace("__DOMAIN__", link.attr("href")
+                .replace("http://","").replace("https://", ""));
+    link.css("background-image", "url(" + url + ")")
+                .addClass(settings.classname);
   });
 };
